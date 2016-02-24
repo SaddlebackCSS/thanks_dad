e577e93365f7900faa57b7b8a5532191259cf906
AnthonyKoutroulis <aakoutroulis@gmail.com>
2016-02-23 18:02:18 -0800

    dumping local files

---
+++ include/header.h
@@ -0,0 +1,8 @@
+//
+// Created by anthony on 11/24/15.
+//
+
+#ifndef THANKS_DAD_HEADER_H
+#define THANKS_DAD_HEADER_H
+
+#endif //THANKS_DAD_HEADER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,139 @@
+
+#include <Thor/Particles.hpp>
+#include <Thor/Animations.hpp>
+#include <Thor/Vectors/PolarVector2.hpp>
+#include <Thor/Math/Distributions.hpp>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200,200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // Create window
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Thor Particles");
 
-    while(window.isOpen())
+    // Load texture
+    sf::Texture texture;
+    sf::Texture texture2;
+    sf::Texture texture3;
+    sf::Texture texture4;
+
+    const int SIZE = 4;
+    int index = 0;
+
+    if (!texture.loadFromFile("/home/anthony/git/thanks_dad/assets/turkey.png"))
+        return EXIT_FAILURE;
+
+    if (!texture2.loadFromFile("/home/anthony/git/thanks_dad/assets/pie.png"))
+        return EXIT_FAILURE;
+
+    if (!texture3.loadFromFile("/home/anthony/git/thanks_dad/assets/turkey_plate.png"))
+        return EXIT_FAILURE;
+
+    if (!texture4.loadFromFile("/home/anthony/git/thanks_dad/assets/tiger.png"))
+        return EXIT_FAILURE;
+
+    sf::Texture textureAr[SIZE] = { texture, texture2, texture3, texture4};
+
+
+      // Create emitter
+    thor::UniversalEmitter emitter;
+    emitter.setEmissionRate(10.f);
+    emitter.setParticleLifetime(sf::seconds(5.f));
+
+    // Create particle system, add reference to emitter
+    thor::ParticleSystem system;
+    system.setTexture(textureAr[0]);
+    system.addEmitter(thor::refEmitter(emitter));
+
+    // Build color gradient (green -> teal -> blue)
+    thor::ColorGradient gradient;
+    gradient[0.f] = sf::Color(0, 150, 0);
+    gradient[0.5f] = sf::Color(0, 150, 100);
+    gradient[1.f] = sf::Color(0, 0, 150);
+
+    // Create color and fade in/out animations
+    thor::ColorAnimation colorizer(gradient);
+    thor::FadeAnimation fader(0.5f, 0.5f);
+
+    // Add particle affectors
+   // system.addAffector( thor::AnimationAffector(colorizer) );
+    system.addAffector( thor::AnimationAffector(fader) );
+    system.addAffector( thor::TorqueAffector(250.f) );
+    system.addAffector( thor::ForceAffector(sf::Vector2f(0.f, 100.f)) );
+    system.addAffector( thor::ScaleAffector(sf::Vector2f(-1.f, -1.f)));
+
+    // Attributes that influence emitter
+    thor::PolarVector2f velocity(400.f, -90.f);
+    bool paused = false;
+
+    sf::Font font;
+    if (!font.loadFromFile("/home/anthony/git/thanks_dad/assets/sansation.ttf"))
+        return 1;
+
+    // Instruction text
+    sf::Text instructions(
+            "Left click: Pause\n"
+                    "Mouse wheel: Change direction\n",
+            font, 14u);
+
+    // Create clock to measure frame time
+    sf::Clock frameClock;
+
+    // Main loop
+    for (;;)
     {
+        // Handle events
         sf::Event event;
-        while(window.pollEvent(event))
+        while (window.pollEvent(event))
         {
-            if(event.type == sf::Event::Closed)
-                window.close();
+
+            switch (event.type)
+            {
+                // [X]: Quit
+                case sf::Event::Closed:
+                    return 0;
+
+                    // Escape: Quit
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape) {
+                        return 0;
+                    }
+                    else
+                    {
+                        index++;
+                        if(index == SIZE)
+                        {
+                            index = 0;
+                        }
+                        system.clearParticles();
+                        system.setTexture(textureAr[index]);
+                    }
+                    // Left mouse button: Enable/disable glow
+                case sf::Event::MouseButtonPressed:
+                    if (event.mouseButton.button == sf::Mouse::Left)
+                        paused = !paused;
+                    break;
+
+                    // Mouse wheel: Change emission direction
+                case sf::Event::MouseWheelMoved:
+                    velocity.phi += 12.f * event.mouseWheel.delta;
+                    break;
+            }
         }
 
-        window.clear();
-        window.draw(shape);
+        // Update particle system and emitter
+        const sf::Time frameTime = frameClock.restart();
+        if (!paused)
+            system.update(frameTime);
+
+
+        // Set initial particle position and velocity, rotate vector randomly by maximal 10 degrees
+        emitter.setParticlePosition( window.mapPixelToCoords(sf::Mouse::getPosition(window)) );
+        emitter.setParticleVelocity( thor::Distributions::deflect(velocity, 10.f) );
+
+        // Draw everything
+        window.clear(sf::Color(30, 30, 30));
+        window.draw(instructions);
+        window.draw(system);
         window.display();
     }
-
-    return 0;
 }
